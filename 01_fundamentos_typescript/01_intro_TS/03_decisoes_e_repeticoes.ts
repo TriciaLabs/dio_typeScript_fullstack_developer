703165ddd62a23b5c56569f8ec3c20098d6a5160
Patricia Souza <patricia.sz.br@gmail.com>
2026-09-18 20:59:17 -0300

    decisoes e reps

--- 01_fundamentos_typescript/01_intro_TS/03_decisoes_e_repeticoes.ts
+++ 01_fundamentos_typescript/01_intro_TS/03_decisoes_e_repeticoes.ts
@@ -2,6 +2,8 @@ const num: number = 13
 
 if(num > 15) {
   console.log("Num MAIOR que 15");
+}else if(num == 15 ) {
+  console.log("Num IGUAL que 15");
 }else {
   console.log("Num MENOR que 15");
 }
