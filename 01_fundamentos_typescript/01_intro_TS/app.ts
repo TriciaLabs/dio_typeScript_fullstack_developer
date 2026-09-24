5a2ff777a101729ff59d8311fe8a0075496799fd
Patricia Souza <patricia.sz.br@gmail.com>
2026-09-24 19:22:44 -0300

    modulos

--- 01_fundamentos_typescript/01_intro_TS/app.ts
+++ 01_fundamentos_typescript/01_intro_TS/app.ts
@@ -1,7 +1,10 @@
-const num: number = 13
+import {PeopleAccount} from '../class/PeopleAccount'
+import {CompanyAccount} from '../class/CompanyAccount'
 
-if(num > 15) {
-  console.log("Num MAIOR que 15");
-}else {
-  console.log("Num MENOR que 15");
-}
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Tricia', 234) 
+peopleAccount.deposit()
+console.log(peopleAccount);
+
+const companyAccount: CompanyAccount = new CompanyAccount('Dio', 2099)
+companyAccount.deposit()
+console.log(companyAccount);
